baca1e284e00e0ebed73c5fd30be11bc3425a038
Vaibhav <vaibhav.mahajan1509@email.comgit config -listgit config --global user.email vaibhav.mahajan1509@email.com>
2026-06-23 11:59:54 +0530

    Added some new section

--- script.js
+++ script.js
@@ -680,3 +680,24 @@ faqs.forEach((item) => {
     }
   });
 });
+
+// Annimation Effect on Certification
+const cards = document.querySelectorAll('.certificate-gallery-card');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+
+    });
+}, {
+    threshold: 0.1
+});
+
+cards.forEach(card => observer.observe(card));
+
+
